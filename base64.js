ffab9b547b9e8f5ab7b405e5bb73c720c499a1b0
tomli <g13477940@gmail.com>
2019-10-16 17:34:28 +0800

    Update base64.js

--- base64.js
+++ base64.js
@@ -6,4 +6,4 @@
  *
  */
 
-"use strict";var base64=exports;base64.encode=function(e){return new Buffer(e||"").toString("base64")},base64.decode=function(e){return new Buffer(e||"","base64").toString("utf8")},base64.urlEncode=function(e){return base64.encode(e).replace(/\+/g,"-").replace(/\//g,"_").replace(/=+$/,"")},base64.urlDecode=function(e){for(e=e.replace(/-/g,"+").replace(/_/g,"/");e.length%4;)e+="=";return base64.decode(e)};
+"use strict";var base64=window.base64;base64.encode=function(e){return new Buffer(e||"").toString("base64")},base64.decode=function(e){return new Buffer(e||"","base64").toString("utf8")},base64.urlEncode=function(e){return base64.encode(e).replace(/\+/g,"-").replace(/\//g,"_").replace(/=+$/,"")},base64.urlDecode=function(e){for(e=e.replace(/-/g,"+").replace(/_/g,"/");e.length%4;)e+="=";return base64.decode(e)};
