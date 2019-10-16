5e0d776b46525a2edb1389b763abceb3f27dc779
tomli <g13477940@gmail.com>
2019-10-16 17:49:20 +0800

    Update base64.js

--- base64.js
+++ base64.js
@@ -1,28 +1,30 @@
 const encode_regex = /[\+=\/]/g;
 const decode_regex = /[\._\-]/g;
 
-// Buffer -> Base64 String -> Url Safe Base64 String
-export function encode(buffer) {
-  return buffer.toString('base64').replace(encode_regex, encodeChar);
-}
+var website = {};
 
-// Url Safe Base64 String -> Base64 String -> Buffer
-export function decode(string) {
-  return new Buffer(string.replace(decode_regex, decodeChar), 'base64');
-}
+(()=>{
+	website["encode"] = (buffer) => {
+		return buffer.toString('base64').replace(encode_regex, encodeChar);
+	};
+	function encodeChar(c) {
+	  switch (c) {
+	    case '+': return '.';
+	    case '=': return '-';
+	    case '/': return '_';
+	  }
+	}
+})();
 
-function encodeChar(c) {
-  switch (c) {
-    case '+': return '.';
-    case '=': return '-';
-    case '/': return '_';
-  }
-}
-
-function decodeChar(c) {
-  switch (c) {
-    case '.': return '+';
-    case '-': return '=';
-    case '_': return '/';
-  }
-}
+(()=>{
+	website["decode"] = (string) => {
+		return new Buffer(string.replace(decode_regex, decodeChar), 'base64');
+	};
+	function decodeChar(c) {
+	  switch (c) {
+	    case '.': return '+';
+	    case '-': return '=';
+	    case '_': return '/';
+	  }
+	}
+})();
